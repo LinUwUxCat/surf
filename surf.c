607a4616a3796e67bba6e81a2e62349dbc824f80
LinuxCat <masch77.linuxcat@gmail.com>
2023-01-07 18:39:05 +0100

    switch to uint

--- surf.c
+++ surf.c
@@ -1,21 +1,21 @@
 #include "surf.h"
 
-void surfsetseed(int32_t seed){
+void surfsetseed(uint32_t seed){
 	__surf_seed=seed;
 }
 
-int32_t surf(){
+uint32_t surf(){
 	float f=__surf_seed;
-	__surf_seed*=*(int32_t *)&f;
+	__surf_seed*=*(uint32_t *)&f;
 	return __surf_seed++;
 }
 
-void dsurfsetseed(int64_t seed){
+void dsurfsetseed(uint64_t seed){
 	__dsurf_seed=seed;
 }
 
-int64_t dsurf(){
+uint64_t dsurf(){
 	double d=__dsurf_seed;
-	__dsurf_seed*=*(int64_t *)&d;
+	__dsurf_seed*=*(uint64_t *)&d;
 	return __dsurf_seed++;
 }
